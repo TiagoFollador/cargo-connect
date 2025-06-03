09cd17ffeaf9192d3679e096960f5360fb7f016b
TiagoFollador <tiagobritofollador@gmail.com>
2025-06-02 23:28:42 -0300

    :wrench: - database connection

---
+++ backend/db.js
@@ -0,0 +1,26 @@
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 0,
+    queueLimit: 0
+});
+
+const promisePool = pool.promise(); // para usar promises
+
+promisePool.getConnection() // testa a conexao
+  .then(connection => {
+    console.log('Conectado ao banco de dados MySQL com sucesso!');
+    connection.release(); 
+  })
+  .catch(error => {
+    console.error('Erro ao conectar com o banco de dados MySQL:', error);
+  });
+
+module.exports = promisePool;
