6d5e1a52cd214bd59f06775f50c1cde080ced52f
TiagoFollador <tiagobritofollador@gmail.com>
2025-06-02 22:15:41 -0300

    :tada: - start backend

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use('/', useRoutes)
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
